7217889120ffa813aa22b2fd94a3d18557383980
ihsan <iamihsankurban@outlook.com>
2024-01-10 16:07:45 +0000

    89% passed the tests

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -1,56 +1,62 @@
 #include "reassembler.hh"
+#include <cstdint>
 
 using namespace std;
 
 void Reassembler::insert(uint64_t first_index, string data, bool is_last_substring, Writer &output)
 {
-    auto rsmbl_func = [&]() {
-        if (first_index != m_next_currect_index)
-        {
-            // 有空间放置&&当前buffer不存在此数据包
-            if (output.available_capacity() - m_rasmblr_buf_cur_data_num >= data.size() &&
-                m_rasmblr_buffer.find(first_index) == m_rasmblr_buffer.end())
-            {
-                m_rasmblr_buf_cur_data_num += data.size();
-                m_rasmblr_buffer[first_index] = std::move(data);
-            }
-            return;
-        }
+    if (first_index < m_next_currect_index)
+    {
+        return;
+    }
 
-        m_next_currect_index = first_index + 1;
+    uint64_t avbl_cap = output.available_capacity();
+
+    auto l_push = [&]() {
+        m_next_currect_index = first_index + data.size();
         output.push(std::move(data));
-
-        while (!m_rasmblr_buffer.empty())
+        while (m_rasmblr_buffer.find(m_next_currect_index) != m_rasmblr_buffer.end())
         {
-            // 没空间 || 没有顺序合适的数据包
-            if (!output.available_capacity() || m_rasmblr_buffer.find(m_next_currect_index) == m_rasmblr_buffer.end())
-            {
-                break;
-            }
-            // 从 reassembler buffer 总字符数 减去 当前被推到 Write buffer的字符数量
-            m_rasmblr_buf_cur_data_num -= m_rasmblr_buffer[m_next_currect_index].size();
+            // 这样编写比起直接调用两次size更容易被编译器优化
+            auto tmp_size = m_rasmblr_buffer[m_next_currect_index].size();
+            m_rasmblr_buf_cur_data_num -= tmp_size;
+            m_next_currect_index += tmp_size;
 
-            // 推过去下一个数据包，比如这次给送过来的[index 5],
-            // 本来rsmbl_buffer中有 [index 6]那么它也会推过去，在这个循环中
             output.push(std::move(m_rasmblr_buffer[m_next_currect_index]));
-            
-            // 清除当前被推到 Write buffer 的键值对
-            m_rasmblr_buffer.erase(m_next_currect_index++);
-
+            m_rasmblr_buffer.erase(m_next_currect_index);
         }
     };
 
+    // 直接push的情况集合
+    if (first_index == m_next_currect_index)
+    {
+        l_push();
+    }
+
+    auto overlapping = [&]() {
+        if (m_rasmblr_buffer.find(first_index) != m_rasmblr_buffer.end())
+        {
+            m_rasmblr_buffer[first_index] = std::move(data);
+            return;
+        }
+        m_rasmblr_buf_cur_data_num += data.size();
+        m_rasmblr_buffer[first_index] = std::move(data);
+    };
+
+    // 有空间 && 这个index不存在当前buffer
+    if (avbl_cap - m_rasmblr_buf_cur_data_num >= data.size())
+    {
+        overlapping();
+    }
+
     if (is_last_substring)
     {
-        rsmbl_func();
         output.close();
-        return;
     }
-    rsmbl_func();
 }
 
 uint64_t Reassembler::bytes_pending() const
 {
     // Your code here.
-    return {};
+    return m_rasmblr_buf_cur_data_num;
 }
